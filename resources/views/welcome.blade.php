98999b9efd321f2a866c0de7c3eb0de62dbbcf49
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-09 22:46:03 +0900

    アイコン変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 
 <div class="center jumbotron bg-danger ">
     <div class="text-center text-white mt-2 pt-1">
-        <h1><i class="fas fa-chalkboard-teacher pr-3 d-inline"></i>ChatApp</h1>
+        <h1><i class="fas fa-comment-dots pr-3 d-inline"></i>ChatApp</h1>
     </div>
 </div>
 <h5 class="description text-center">チャットで会話をしましょう</h5>
