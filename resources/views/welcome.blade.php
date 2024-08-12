175a347a9e60981317ae74daa0fc4167fe9028c5
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-12 12:24:54 +0900

    新規の投稿を作成

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,4 +7,18 @@
     </div>
 </div>
 <h5 class="description text-center">チャットで会話をしましょう</h5>
+@if (Auth::check())
+    <div class="w-75 m-auto">@include('commons.error_messages')</div>
+    <div class="text-center mb-3-">
+        <form method="post" action="{{ route('posts.store') }}" class="d-inline-block w-75">
+        @csrf
+            <div class="form-group">
+                <textarea class="form-control" name="contents" rows="2">{{ old('contents') }}</textarea>
+                <div class="text-left mt-3">
+                    <button type="submit" class="btn btn-success">投稿する</button>
+                </div>
+            </div>
+        </form>
+    </div>
+@endif
 @endsection
