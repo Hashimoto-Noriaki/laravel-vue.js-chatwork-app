cd46ebfdf41859a4261321673c11e6fe1bbc0a04
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-14 20:02:15 +0900

    論理削除の実装

--- app/User.php
+++ app/User.php
@@ -39,9 +39,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    // protected $dates = [
-    //     'deleted_at'
-    // ];
+    protected $dates = [
+        'deleted_at'
+    ];
 
     public function posts()
     {
