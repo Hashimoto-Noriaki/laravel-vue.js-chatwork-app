9ba857fbf10c6e57424b6eae65c0c83830c6f65d
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-14 13:13:54 +0900

    投稿を編集して更新する

--- routes/web.php
+++ routes/web.php
@@ -37,8 +37,11 @@ Route::prefix('users')->group(function() {
 
 // ログイン後
 Route::group(['middleware' => 'auth'], function () {
-    // 動画
+    // 投稿
     Route::prefix('posts')->group(function () {
         Route::post('', 'PostsController@store')->name('posts.store');
+        Route::get('{id}/edit','PostsController@edit')->name('posts.edit');
+        Route::put('{id}','PostsController@update')->name('posts.update');
+        Route::delete('{id}', 'PostsController@destroy')->name('post.delete');
     });
 });
