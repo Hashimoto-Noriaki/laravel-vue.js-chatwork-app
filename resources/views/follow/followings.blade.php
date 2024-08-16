ffa6769311f9146481d09b4f7edbce15ad5aaac7
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-16 17:19:44 +0900

    フォロワーとフォロー中を確認できる

--- resources/views/follow/followings.blade.php
+++ resources/views/follow/followings.blade.php
@@ -12,7 +12,7 @@
             @foreach ($followings as $follow)
                 <li class="mb-3 text-center">
                     <div class="text-left d-inline-block w-75 mb-2">
-                        <img class="mr-2 rounded-circle" src="{{ Gravatar::src($follow->email, 55) }}" alt="ユーザのアバター画像">
+                        <img class="mr-2 rounded-circle" src="{{ Gravatar::get($follow->email, ['size' => 55]) }}" alt="ユーザのアバター画像">
                         <p class="mt-3 mb-0 d-inline-block"><a href="{{ route('users.show',$follow->id) }}">{{$follow->name}}</a></p>
                     </div>
                 </li>
